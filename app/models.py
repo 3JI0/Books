a248be647bb28945b7db3c256aaf33fc2f858862
alexey zezin <alexey.zezin@gmail.com>
2023-10-18 14:28:15 +0300

    add db on index

--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(64), index=True, unique=False)
-    books= db.Column(db.String(140))
+    books = db.Column(db.String(140))
    
    
     def __repr__(self):
-        return '<Author: {}>'.format(self.author)
+        return '<Author: {}, Books: {}>'.format(self.author, self.books)
+    
--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,12 @@ def make_shell_context():
 @app.route('/index')
 def index():
     content = {'Book': 'books'}
-    list_books = [{'author':'a', 'book':'b'}, {'author': 'c', 'book':'d'}]
-    return render_template('index.html', title='Home', content=content, list_books=list_books)
+    #list_books = [{'author':'a', 'book':'b'}, {'author': 'c', 'book':'d'}]
+
+    user = User.query.all()
+
+        
+    return render_template('index.html', title='Home', content=content, user=user)
 
 @app.route('/books', methods = ['GET', 'POST'])
 def books():
@@ -26,7 +30,9 @@ def books():
     form = BookForm()
     if form.validate_on_submit():
         user = User(author=form.author.data, books=form.books.data)
-        list_books[form.author.data] = form.books.data
+        db.session.add(user)
+        db.session.commit()
+    #    list_books[form.author.data] = form.books.data
         flash('Book  {}, by author {} is added'.format(
              form.author.data, form.books.data))
         return redirect(url_for('index'))
@@ -36,19 +42,11 @@ def books():
 @app.route('/json', methods = ['GET'])
 def json(): 
     if(request.method == 'GET'): 
-        list_books = {}
-        form = BookForm()
-       # user = User(author=form.author.data, books=form.books.data)
-        list_books[form.author.data] = form.books.data
-        #connection = lite.connect('app.db')
-        #cursor = connection.cursor()
-        #cursor.execute('SELECT * FROM User')
-        #users = cursor.fetchall()
-        #for row in users:
-        #    name = str(row[1])
-        #    book = str(row[0])
-        #   return name, book
-        list_books = [{'a':'a', 'b':'b'}, {'a': 'c', 'b':'d'}]
+        user = User.query.all()
+        list_books = []
+        for u in user:
+            dict_books = {'Author':u.author, 'Books':u.books}  
+            list_books.append(dict_books)
         return  jsonify( list_books) 
     return render_template('json.html', title='JSON')  
 
